17ef3843599075ee1d522f76a9b692f69a1b15fd
Tran <trakhak@dunwoody.edu>
2019-10-27 21:10:37 -0500

    output data

--- Program.cs
+++ Program.cs
@@ -14,26 +14,39 @@ namespace assignment_three
         {
             //Declarations
             int EXIT = -1;
-            int name, depName;
-            int depNum, hoursWork;
-            double gross, salary;
+            string employeeName = "";
+            int deptNum;
+            double hourlyPay, numOfHoursWorked, salary = 0.0;
+            double [] deptSalaryAccum = {0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+            string [] DEPT_NAMES = {"Personnel", "Marketing", "Information Technology", "Computer Services", "Sales", "Legal", "Accounting"};
+
             Console.WriteLine("MPLS Dog Boarding HR Payroll Report Program");
-            Console.WriteLine("Please enter your name");
-            name = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter department number");
+            deptNum = Convert.ToInt32(Console.ReadLine());
 
-            while(name != EXIT){
-                //defensive programming -- something that only accepts characters
-                while(name = ){
+            while(deptNum != EXIT){
 
-                    name = Convert.ToInt32(Console.ReadLine());
-                }
+                Console.WriteLine("Please enter name");
+                employeeName = Console.ReadLine();
+
+                Console.WriteLine("Please enter hourly pay");
+                hourlyPay = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter hours worked");
+                numOfHoursWorked = Convert.ToInt32(Console.ReadLine());
+
+                salary = hourlyPay * numOfHoursWorked;
                 
-                Console.WriteLine($"Welcome {name}");
-                Console.WriteLine("Enter department number");
-                depNum = Console.ReadLine()
+                //output
+                Console.WriteLine($"For {employeeName}, salary is {salary} department is {DEPT_NAMES[deptNum - 1]}");
+                Console.WriteLine("thank you for using mpls dog boarding hr payroll report program");
 
-            }
+                //primer
+                Console.WriteLine("To continue, please enter another department number or enter -1 to exit the program");
+                deptNum = Convert.ToInt32(Console.ReadLine());
 
+
+            }            
             
         }
     }
